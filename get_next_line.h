e1902174c5e420afe7f0ffda9cf173131e9a0ab4
Keorapetse MPOLOKENG <kmpoloke@c4r7s9.wethinkcode.co.za>
2019-07-09 13:00:37 +0200

    get_next_line_project

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmpoloke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/05 12:43:23 by kmpoloke          #+#    #+#             */
+/*   Updated: 2019/07/08 17:15:08 by kmpoloke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 2
+
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
